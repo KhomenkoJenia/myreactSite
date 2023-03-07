a78c771814763d88f3ae0cc19473f3df181bd869
JeniaKhomenko <jeniaaajeniaaa@gmail.com>
2023-03-07 22:42:31 +0200

    five

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import Header from "./Components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./scss/variables.scss";
+
 import "./scss/fonts.scss";
 import "./scss/templane.scss";
+import "./scss/media/_media_max_width991.scss";
 
 function App() {
   return (
